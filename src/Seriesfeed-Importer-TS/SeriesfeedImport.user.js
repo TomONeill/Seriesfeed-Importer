16452390654b2bb0ad88ca839d2a687d2ca9c970
Tom O'Neill <TomONeill@github.com>
2017-11-18 13:02:33 +0100

    StyleService update.

--- src/Seriesfeed-Importer-TS/SeriesfeedImport.user.js
+++ src/Seriesfeed-Importer-TS/SeriesfeedImport.user.js
@@ -3053,96 +3053,68 @@ var SeriesfeedImporter;
     (function (Services) {
         class StyleService {
             static loadGlobalStyle() {
-                const css = '<style>'
-                    + '    .import-selected {'
-                    + '        border-bottom: 3px solid #447C6F;'
-                    + '    }'
-                    + '    input[type="checkbox"] + label span {'
-                    + '        margin-top: -3px;'
-                    + '    }'
-                    + '    '
-                    + '    fieldset {'
-                    + '        margin-top: 0px !important;'
-                    + '    }'
-                    + '    '
-                    + '    .progress {'
-                    + '        width: 90%;'
-                    + '        margin: 0 auto;'
-                    + '    }'
-                    + '    '
-                    + '    .progress-bar {'
-                    + '        background: #447C6F;'
-                    + '    }'
-                    + '    '
-                    + '    @media only screen and (max-width: 992px)'
-                    + '    .favourites i.fa.fa-times:hover:after {'
-                    + '        position: fixed;'
-                    + '        top: 50px;'
-                    + '        left: 10px;'
-                    + '        right: 10px;'
-                    + '    }'
-                    + '    '
-                    + '    .favourites i.fa.fa-times:hover:after {'
-                    + '        content: "Deze serie staat nog niet op Seriesfeed. Vraag \'m aan via menu-item Series -> Serie voorstellen.";'
-                    + '        background: #ffffff;'
-                    + '        position: absolute;'
-                    + '        min-width: 250px;'
-                    + '        padding: 20px;'
-                    + '        font-family: "Lato", sans-serif;'
-                    + '        border: 1px solid #eaeaea;'
-                    + '        border-radius: 3px;'
-                    + '        box-shadow: 0 2px 3px 0 rgba(0, 0, 0, 0.15);'
-                    + '        line-height: 18px;'
-                    + '    }'
-                    + '    '
-                    + '    tr.row-error {'
-                    + '        background-color: rgba(255, 0, 0, 0.15);'
-                    + '    }'
-                    + '    '
-                    + '    tr.row-warning {'
-                    + '        background-color: rgba(255, 231, 150, 0.43);'
-                    + '    }'
-                    + '    '
-                    + '    tr.row-info {'
-                    + '        background-color: rgba(240, 248, 255, 1.00);'
-                    + '    }'
-                    + ''
-                    + '    .fa-flip-x {'
-                    + '        animation-name: flipX;'
-                    + '        animation-duration: 2s;'
-                    + '        animation-iteration-count: infinite;'
-                    + '        animation-direction: alternate;'
-                    + '        animation-timing-function: ease-in-out;'
-                    + '    }'
-                    + ''
-                    + '    @keyframes flipX {'
-                    + '        0% { transform: rotateX(0); }'
-                    + '        50% { transform: rotateX(180deg); }'
-                    + '    }'
-                    + ''
-                    + '    .fa-flip-y {'
-                    + '        animation-name: flipY;'
-                    + '        animation-duration: 2s;'
-                    + '        animation-iteration-count: infinite;'
-                    + '        animation-direction: alternate;'
-                    + '        animation-timing-function: ease-in-out;'
-                    + '    }'
-                    + ''
-                    + '    @keyframes flipY {'
-                    + '        0% { transform: rotateY(0); }'
-                    + '        50% { transform: rotateY(180deg); }'
-                    + '    }'
-                    + ''
-                    + '    .table.thicken-header thead {'
-                    + '        border-bottom: 2px solid #d9d9d9;'
-                    + '    }'
-                    + ''
-                    + '    .brackets {'
-                    + '        text-rendering: auto;'
-                    + '        -webkit-font-smoothing: antialiased;'
-                    + '        -moz-osx-font-smoothing: grayscale;'
-                    + '    }'
-                    + '</style>';
+                const css = `<style>
+            input[type="checkbox"] + label span {
+                margin-top: -3px;
+            }
+
+            fieldset {
+                margin-top: 0px !important;
+            }
+
+            .progress {
+                width: 90%;
+                margin: 0 auto;
+            }
+
+            .progress-bar {
+                background: #447C6F;
+            }
+
+            .fa-flip-x {
+                animation-name: flipX;
+                animation-duration: 2s;
+                animation-iteration-count: infinite;
+                animation-direction: alternate;
+                animation-timing-function: ease-in-out;
+            }
+
+            @keyframes flipX {
+                0% {
+                    transform: rotateX(0);
+                }
+                50% {
+                    transform: rotateX(180deg);
+                }
+            }
+
+            .fa-flip-y {
+                animation-name: flipY;
+                animation-duration: 2s;
+                animation-iteration-count: infinite;
+                animation-direction: alternate;
+                animation-timing-function: ease-in-out;
+            }
+
+            @keyframes flipY {
+                0% {
+                    transform: rotateY(0);
+                }
+                50% {
+                    transform: rotateY(180deg);
+                }
+            }
+
+            .table.thicken-header thead {
+                border-bottom: 2px solid #d9d9d9;
+            }
+
+            .brackets {
+                text-rendering: auto;
+                -webkit-font-smoothing: antialiased;
+                -moz-osx-font-smoothing: grayscale;
+            }
+        </style>`;
                 $('body').append(css);
             }
         }
